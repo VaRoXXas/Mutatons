a799eadc6b423b2a86a3daf787fc3f056269d271
GRMv1 <64690277+GRMv1@users.noreply.github.com>
2021-03-17 18:59:21 +0100

    update map

---
+++ Unity_ver/Assets/Scripts/KillingTile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillingTile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        Creature mutaton = collider.GetComponent<Creature>();
+        Destroy(mutaton.gameObject);
+           
+    }
+}
