90582c42a3f1804f81db417c9b30ea99c45d8533
GRMv1 <64690277+GRMv1@users.noreply.github.com>
2021-04-10 19:11:23 +0200

    adding DataManager class

---
+++ OpenGL_ver/include/DataManager.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "pch.h"
+
+
+class DataManager
+{
+public:
+
+    DataManager();
+    ~DataManager();
+    unsigned int* LoadAllTextures();
+    unsigned int LoadTexture(char const* path);
+    void LoadPlayBackgroundMusic(char const* path, sf::Music &music);
+    void LoadSound(char const* path, sf::Sound &sound, sf::SoundBuffer &buffer);
+};
---
+++ OpenGL_ver/include/FileNames.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define CONTAINER_DIFFUSE "res/textures/container_diffuse.png"
+#define GRASS_DIFFUSE "res/textures/grass_diffuse.jpg"
+#define FAJNA_MUZYCZKA "res/music/Intergalactic Odyssey.ogg"
