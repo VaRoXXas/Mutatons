24d1260e3021d9f26f02bb1611b85d7a2c1598f8
Krzysztof Michniewski <224377@edu.p.lodz.pl>
2021-04-28 14:17:48 +0200

    Poziom opisany w XMLu

--- OpenGL_ver/src/ObjectTags.cpp
+++ OpenGL_ver/src/ObjectTags.cpp
@@ -4,23 +4,23 @@
 
 
 //variables used to describe game object in file
-const std::string ObjectTags::START_OF_OBJECT = "[START OBJECT]";
-const std::string ObjectTags::END_OF_OBJECT = "[END OBJECT]";
-const std::string ObjectTags::COMPONENT = "[COMPONENT]";
-const std::string ObjectTags::COMPONENT_END = "[/COMPONENT]";
-const std::string ObjectTags::TAG = "[TAG]";
-const std::string ObjectTags::TAG_END = "[/TAG]";
-const std::string ObjectTags::X = "[X]";
-const std::string ObjectTags::X_END = "[/X]";
-const std::string ObjectTags::Y = "[Y]";
-const std::string ObjectTags::Y_END = "[/Y]";
-const std::string ObjectTags::Z = "[Z]";
-const std::string ObjectTags::Z_END = "[/Z]";
-const std::string ObjectTags::SCALE = "[SCALE]";
-const std::string ObjectTags::SCALE_END = "[/SCALE]";
-const std::string ObjectTags::ROTATION = "[ROTATION]";
-const std::string ObjectTags::ROTATION_END = "[/ROTATION]";
-const std::string ObjectTags::ROTATION_AXIS = "[ROTATION AXIS]";
-const std::string ObjectTags::ROTATION_AXIS_END = "[/ROTATION AXIS]";
-const std::string ObjectTags::MODEL_ID = "[MODEL ID]";
-const std::string ObjectTags::MODEL_ID_END = "[/MODEL ID]";
+const std::string ObjectTags::START_OF_OBJECT = "<OBJECT>";
+const std::string ObjectTags::END_OF_OBJECT = "</OBJECT>";
+const std::string ObjectTags::COMPONENT = "<COMPONENT>";
+const std::string ObjectTags::COMPONENT_END = "</COMPONENT>";
+const std::string ObjectTags::TAG = "<TAG>";
+const std::string ObjectTags::TAG_END = "</TAG>";
+const std::string ObjectTags::X = "<X>";
+const std::string ObjectTags::X_END = "</X>";
+const std::string ObjectTags::Y = "<Y>";
+const std::string ObjectTags::Y_END = "</Y>";
+const std::string ObjectTags::Z = "<Z>";
+const std::string ObjectTags::Z_END = "</Z>";
+const std::string ObjectTags::SCALE = "<SCALE>";
+const std::string ObjectTags::SCALE_END = "</SCALE>";
+const std::string ObjectTags::ROTATION = "<ROTATION>";
+const std::string ObjectTags::ROTATION_END = "</ROTATION>";
+const std::string ObjectTags::ROTATION_AXIS = "<ROTATION_AXIS>";
+const std::string ObjectTags::ROTATION_AXIS_END = "</ROTATION_AXIS>";
+const std::string ObjectTags::MODEL_ID = "<MODEL_ID>";
+const std::string ObjectTags::MODEL_ID_END = "</MODEL_ID>";
